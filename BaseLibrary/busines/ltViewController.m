307cc45060959c24499c45f4e64e533b2d232d11
liutong <liutong667@foxmail.com>
2017-09-24 16:30:03 +0800

    add

---
+++ BaseLibrary/busines/ltViewController.m
@@ -0,0 +1,37 @@
+//
+//  ltViewController.m
+//  BaseLibrary
+//
+//  Created by liutong on 2017/9/24.
+//  Copyright © 2017年 liutong. All rights reserved.
+//
+
+#import "ltViewController.h"
+
+@interface ltViewController ()
+
+@end
+
+@implementation ltViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
